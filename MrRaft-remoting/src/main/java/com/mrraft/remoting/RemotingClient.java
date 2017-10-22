652808b5fd5c2eddc07df79ddf5a31448ab0f2a6
mr253727942 <ma.rong@nexuslink.cn>
2017-10-23 00:12:52 +0800

    init

--- MrRaft-remoting/src/main/java/com/mrraft/remoting/RemotingClient.java
+++
@@ -1,7 +0,0 @@
-package com.mrraft.remoting;
-
-/**
- * Created by T460P on 2016/12/30.
- */
-public class RemotingClient {
-}
--- MrRaft-remoting/src/main/java/com/mrraft/remoting/netty/NettyEventType.java
+++
@@ -1,8 +0,0 @@
-package com.mrraft.remoting.netty;
-
-/**
- * Created by T460P on 2016/12/30.
- */
-public enum NettyEventType {
-    CONNECT,CLOSE,IDLE,EXCEPTION
-}
